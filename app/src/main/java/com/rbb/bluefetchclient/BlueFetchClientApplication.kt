6e7ee1f1688288593a92f26413a7f4529dbae29e
rennerbernabe <rennerbrian.bernabe@gmail.com>
2024-11-22 06:39:35 +0800

    Apply Hilt

---
+++ app/src/main/java/com/rbb/bluefetchclient/BlueFetchClientApplication.kt
@@ -0,0 +1,7 @@
+package com.rbb.bluefetchclient
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BlueFetchClientApplication: Application()
--- app/src/main/java/com/rbb/bluefetchclient/ui/MainActivity.kt
+++ app/src/main/java/com/rbb/bluefetchclient/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.rbb.bluefetchclient
+package com.rbb.bluefetchclient.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.rbb.bluefetchclient.ui.theme.BlueFetchClientTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
