0d5dad43e17f4b0747ad1e1e55e5f5d7f47a41ae
rennerbernabe <rennerbrian.bernabe@gmail.com>
2024-11-24 01:23:14 +0800

    Add login validation

--- app/src/main/java/com/rbb/bluefetchclient/ui/screens/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/rbb/bluefetchclient/ui/screens/auth/login/LoginViewModel.kt
@@ -5,9 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.rbb.bluefetchclient.data.AuthRepository
 import com.rbb.bluefetchclient.domain.Credentials
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
